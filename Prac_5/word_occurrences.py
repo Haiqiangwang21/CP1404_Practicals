6010ef138fb761581a93415fc11b6493417a099e
SCP-192 <68849087+SCP-192@users.noreply.github.com>
2020-09-03 15:53:02 +1000

    "Word Counter"

--- Prac_5/word_occurrences.py
+++ Prac_5/word_occurrences.py
@@ -1,15 +1,13 @@
-from collections import Counter
-
-word_occurrences = {}
+word_ot_count = {}
 text_input = input("Enter a string: ")
 words = text_input.split()
 
 for word in words:
-    count = word_occurrences.get(word, 0)
-    word_occurrences[word] = count + 1
+    count = word_ot_count.get(word, 0)
+    word_ot_count[word] = count + 1
 
-words = Counter(words).keys()
+words = list(word_ot_count.keys())
 
-longest_word = max((len(word) for word in words))
+longest_word = max(len(word) for word in words)
 for word in words:
-    print("{:{}} : {} ".format(word, longest_word, word_occurrences[word]))
+    print("{:{}} : {} ".format(word, longest_word, word_ot_count[word]))
